cbdc98b58ca530f39e7f8bdd9fe69b6436daf7cd
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-12-01 18:17:00 +0900

    関数の切り分け2

--- day.js
+++ day.js
@@ -2,3 +2,7 @@ function dayName(date){
   const dayOfTheWeek = ["日曜日","月曜日","火曜日","水曜日","木曜日","金曜日","土曜日"];
   return dayOfTheWeek[date.getDay()];
 }
+
+function greeting(date){
+  return `hello, world! Have a nice ${dayName(date)}`;
+};
