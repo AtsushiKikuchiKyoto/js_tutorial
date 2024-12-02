87b120e32ea19c5e01660a7449afe25e10341501
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-12-02 10:43:00 +0900

    練習問題indexOf

--- function.js
+++ function.js
@@ -46,3 +46,9 @@ function functionalSingles(elements){
   return elements.filter(element => element.split(/\s+/).length === 1);
 };
 console.log(functionalSingles(states));
+
+// filter() ,練習問題：Dakota抽出
+function filterByString(elements, string){
+  return elements.filter(element => element.indexOf(string) > 0);
+};
+console.log(filterByString(states, "Dakota"));
